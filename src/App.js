4b99aac82c5415b22543e408316dbe5da842c642
Телицын Никита <dmtvn.labs@yandex.ru>
2022-04-04 15:16:32 +0300

    Added routing and changed reactDOM.render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Navbar from "./components/navbar/Navbar";
+import Buy from "./pages/buy/Buy";
+import Sell from "./pages/sell/Sell";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/buy" element={<Buy/>}/>
+          <Route path="/sell" element={<Sell/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
