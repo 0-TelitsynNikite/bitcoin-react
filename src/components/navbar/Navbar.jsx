e7d4b1d0c1541c95e54b715dc3a9a61bb8e3403c
Телицын Никита <dmtvn.labs@yandex.ru>
2022-04-04 15:35:15 +0300

    Added border-bottom for Link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,12 +2,20 @@ import './navbar.scss'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+    const changeClass = (e) => {
+        if (e.target.classList.contains('navbar__link')) {
+            e.target.parentNode.childNodes.forEach(elem => elem.classList.remove('active'))
+            e.target.classList.add('active')
+        }
+    }
+
     return (
         <div className='navbar'>
             <i className='icon bi bi-currency-bitcoin'></i>
             <h1 className="title">Купить биткоин</h1>
-            <div className='navbar-wrapper'>
-                <Link className='navbar__link' to="/buy">Купить</Link>
+            <div onClick={(e) => changeClass(e)} className='navbar-wrapper'>
+                <Link className='navbar__link active' to="/buy">Купить</Link>
                 <Link className='navbar__link' to="/sell">Продать</Link>
             </div>
         </div>
